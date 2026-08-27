d06737ad4bf107a764ef100c953497b144bec77c
Nasccped <pedro.main.tec@gmail.com>
2026-08-27 14:33:46 -0300

    chore: mapear request para página inicial

--- project/src/main/java/com/nasccped/pitccexercise/controllers/HomeController.java
+++ project/src/main/java/com/nasccped/pitccexercise/controllers/HomeController.java
@@ -1,26 +1,22 @@
 package com.nasccped.pitccexercise.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * This class refers to the home path controller (like 'http://localhost:8080/').
  *
  * Since most users commonly open the api service at root path ('/'). It would be cool if the root
  * path does some kind of salution. */
-@RestController
+@Controller
 @RequestMapping("/")
 public class HomeController {
 
     /**
-     * Just prints 'welcome home'.
-     * 
-     * TODO: add home page (jsp) redirection.
-     *
-     * */
+     * Resolves to the `index.jsp` page. */
     @GetMapping
     public String home() {
-        return "Welcome home!";
+        return "index";
     }
 }
