317cd5825bd864665de4bf45b3f3675d0da096ff
Nasccped <pedro.main.tec@gmail.com>
2026-08-26 21:29:22 -0300

    chore: inicializar código fonte do projeto

---
+++ project/src/main/java/com/nasccped/pitccexercise/PitccexerciseApplication.java
@@ -0,0 +1,13 @@
+package com.nasccped.pitccexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitccexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitccexerciseApplication.class, args);
+	}
+
+}
---
+++ project/src/test/java/com/nasccped/pitccexercise/PitccexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasccped.pitccexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitccexerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
