e535d16b6eae2e6583c981581c5ce2ef6a114179
Nasccped <pedro.main.tec@gmail.com>
2026-08-27 14:31:34 -0300

    chore: formatar para '4 spaces'

--- project/src/main/java/com/nasccped/pitccexercise/PitccexerciseApplication.java
+++ project/src/main/java/com/nasccped/pitccexercise/PitccexerciseApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PitccexerciseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PitccexerciseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PitccexerciseApplication.class, args);
+    }
 
 }
